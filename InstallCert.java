479c8d0adfae9cc678c3e02f9ec94d1d486c5add
vivekjuneja@gmail.com <vivekjuneja@gmail.com>
2015-05-21 14:00:34 +0900

    Made changes to the getAcceptedIssuers

--- InstallCert.java
+++ InstallCert.java
@@ -179,7 +179,14 @@ public class InstallCert {
         }
 
         public X509Certificate[] getAcceptedIssuers() {
-            throw new UnsupportedOperationException();
+	   
+	    /** 
+	     * This change has been done due to the following resolution advised for Java 1.7+
+		http://infposs.blogspot.kr/2013/06/installcert-and-java-7.html
+       	     **/ 
+	    
+	    return new X509Certificate[0];	
+            //throw new UnsupportedOperationException();
         }
 
         public void checkClientTrusted(X509Certificate[] chain, String authType)
